a65fc6c2adf7313388b5b2f288e888c584e750c4
growlingchaos <growlingchaos@gmail.com>
2016-11-15 21:20:46 +0100

    Initial commit

---
+++ src/main/java/org/damage/UpdateMsgApplication.java
@@ -0,0 +1,12 @@
+package org.damage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateMsgApplication.class, args);
+	}
+}
