cd1d760fde79efb49398f7e0b1a4cff181c2ca88
growlingchaos <growlingchaos@gmail.com>
2016-11-15 21:25:49 +0100

    Reformat

--- src/main/java/org/damage/UpdateMsgApplication.java
+++ src/main/java/org/damage/UpdateMsgApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UpdateMsgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UpdateMsgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateMsgApplication.class, args);
+    }
 }
