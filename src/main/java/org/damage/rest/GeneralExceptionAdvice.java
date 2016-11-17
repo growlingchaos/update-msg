9a0b90ddb1cd96668547e2019860a28943e54fce
Giuseppe Pavan <giuseppe.pavan@iks.it>
2016-11-17 14:43:07 +0100

    Used custom message to handle IAE

--- src/main/java/org/damage/rest/GeneralExceptionAdvice.java
+++ src/main/java/org/damage/rest/GeneralExceptionAdvice.java
@@ -4,6 +4,7 @@ package org.damage.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,8 @@ import java.io.IOException;
 public class GeneralExceptionAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public void handleConflict(IllegalArgumentException e, HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CustomErrorResponse handleConflict(IllegalArgumentException e, HttpServletResponse response) throws IOException {
+        return new CustomErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 }
